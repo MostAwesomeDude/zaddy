d837e58f69af00752860eb0c72d56598e3148023
Corbin <cds@corbinsimpson.com>
2025-04-19 09:50:59 -0700

    rmeta2: Fix off-by-one in error reporting.

--- rmeta2/rmeta2.py
+++ rmeta2/rmeta2.py
@@ -7,13 +7,10 @@ from zaddy import PROGRAMParser
 
 class Slicer(object):
     i = 0
-    line = 1
     def __init__(self, s): self.s = s
     def get(self): return self.s[self.i]
     def eatWhitespace(self):
-        while self.i < len(self.s) and self.s[self.i] in " \n":
-            if self.s[self.i] == '\n': self.line += 1
-            self.i += 1
+        while self.i < len(self.s) and self.s[self.i] in " \n": self.i += 1
     def matches(self, token):
         stop = self.i + len(token)
         if stop > len(self.s): return False
@@ -29,7 +26,7 @@ def main(argv):
         parser.parse(slicer)
         return 0
     except ValueError:
-        line = slicer.s.count("\n", 0, slicer.i)
+        line = slicer.s.count("\n", 0, slicer.i) + 1
         stderr.write("Error at input location: %d (line %d)\n" % (slicer.i, line))
         stderr.write("Backtrace: %s\n" %
                      " ".join([frame[0] for frame in parser.stack]))
